be82de838ca6e16dc6da54b6cc55963809fcc9c6
Vidit <vidit24@outlook.com>
2021-09-13 17:04:38 +0530

    added like functionality

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://vidit:vidit@2410@cluster0.77thc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://rajreeturaj:pizUxQnMgbSU7xD@cluster0.jktsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology:true})
