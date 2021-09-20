b0e086103be965d1f37efa7991b170f9e6f667d4
Vidit <vidit24@outlook.com>
2021-09-20 16:55:07 +0530

    mobile responsive

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from "dotenv";
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit:"30mb", extended : true}));
@@ -16,11 +17,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vidit:vidit@2410@cluster0.77thc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//const CONNECTION_URL = 'mongodb+srv://vidit:vidit@2410@cluster0.77thc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 // const CONNECTION_URL = 'mongodb+srv://rajreeturaj:pizUxQnMgbSU7xD@cluster0.jktsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology:true})
 .then(()=>app.listen(PORT, ()=>console.log(`Server running on port :${PORT}`)))
 .catch((error)=> console.log(error.message));
 
